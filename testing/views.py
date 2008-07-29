f4d31e386c04bb204ed62e3da8d2b5d308ed5830
versae <versae@75a36c7b-de52-0410-b93c-6f98b1bda94e>
2008-07-29 23:43:12 +0000

    Draft.

--- testing/views.py
+++ testing/views.py
@@ -4,9 +4,12 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.utils.translation import gettext as _
 
+from django_orbited.client import Client
 
 #@login_required
 def index(request):
+    client = Client("update", user=request.user, session_data=request.session.session_key)
+
     return render_to_response('test.html',
                               {},
                               context_instance=RequestContext(request))
