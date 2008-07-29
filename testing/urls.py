f790072d7147931cfc8fee4b64b41d51d0a33add
versae <versae@75a36c7b-de52-0410-b93c-6f98b1bda94e>
2008-07-29 00:13:07 +0000

    First dirty implementation.

--- testing/urls.py
+++ testing/urls.py
@@ -1,16 +1,23 @@
+from os import path
 from django.conf.urls.defaults import *
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.conf import settings
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^testing/', include('testing.foo.urls')),
+    # Static content
+    (r'^static/(.*)$', 'django.views.static.serve', {'document_root': path.join(settings.BASEDIR, 'media')}),
+    (r'^orbited/', include('django_orbited.urls')),
 
-    # Uncomment the next line to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # Admin documentation
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line for to enable the admin:
-    # (r'^admin/(.*)', admin.site.root),
+    # Django contrib
+    (r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
+    (r'^logout/$', 'django.contrib.auth.views.logout'),
+    (r'^admin/(.*)', admin.site.root),
+
+    # Default
+    (r'^$', 'views.index'),
+
 )
