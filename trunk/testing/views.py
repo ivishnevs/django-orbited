1d10f7ca26c16e4897e429e84b24c08d343cd3ca
versae <versae@75a36c7b-de52-0410-b93c-6f98b1bda94e>
2008-08-03 23:24:04 +0000

    Fixed minor errors.

--- trunk/testing/views.py
+++ trunk/testing/views.py
@@ -7,9 +7,7 @@ from django.utils.translation import gettext as _
 from django_orbited.client import Client
 
 
-#@login_required
 def index(request):
-#    print request.user
     return render_to_response('test.html',
                               {},
                               context_instance=RequestContext(request))
